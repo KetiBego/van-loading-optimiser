c5b5d43ae6ee300bfd90f3cbf2c5a6d4cb3c235e
KetiBego <kbegi20@freeuni.edu.ge>
2026-05-05 20:26:37 +0400

    Initial commit: setup

---
+++ src/main/java/com/ketevan/vanloadingoptimiser/VanloadingoptimiserApplication.java
@@ -0,0 +1,13 @@
+package com.ketevan.vanloadingoptimiser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VanloadingoptimiserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VanloadingoptimiserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ketevan/vanloadingoptimiser/VanloadingoptimiserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ketevan.vanloadingoptimiser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VanloadingoptimiserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
